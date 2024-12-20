240534f0e3717fe34d1cc3799f639993da0e7c13
Arvin Buid <arvinbuid01@gmail.com>
2024-12-20 16:43:37 +0800

    Update linkedin link.

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -46,7 +46,7 @@ const Contact = () => {
           </a>
           {/* linkedin */}
           <a
-            href='https://www.linkedin.com/in/arvin-buid/'
+            href='https://www.linkedin.com/in/arvinbuid01'
             className='inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground hover:bg-secondary/80 h-10 px-4 py-2 hover:text-blue-600'
           >
             <div className='flex items-center justify-center'>
