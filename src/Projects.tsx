99d27884861723b639f90defa6ff81cfbb1f620a
Arvin Buid <arvinbuid01@gmail.com>
2025-03-26 15:02:43 +0800

    Added hirepulse project.

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,8 +1,8 @@
 import theWildOasis from "./assets/the-wild-oasis.png";
-import natours from "./assets/natours.png";
 import nextchat from "./assets/next-chat.png";
 import dapperfit from "./assets/dapper-fit.png";
 import workhive from "./assets/workhive.png";
+import hirepulse from "./assets/hirepulse.png";
 
 import HTML from "./assets/techstack/html5.png";
 import CSS3 from "./assets/techstack/css3.png";
@@ -10,7 +10,6 @@ import JS from "./assets/techstack/javascript.svg";
 import Typescript from "./assets/techstack/typescript.svg";
 import NodeJS from "./assets/techstack/nodejs.svg";
 import Express from "./assets/techstack/express.png";
-import Pug from "./assets/techstack/pug.svg";
 import MongoDB from "./assets/techstack/mongodb.png";
 import React from "./assets/techstack/react.svg";
 import NextJS from "./assets/techstack/nextjs.svg";
@@ -387,26 +386,26 @@ const Projects = () => {
           </div>
         </aside>
 
-        {/* next-chat */}
+        {/* hirepulse */}
         <aside className='flex flex-col border rounded overflow-hidden h-auto w-full shadow m-auto max-w-[300px]'>
           <div className='relative h-[180px] overflow-hidden flex justify-center'>
             <img
-              src={nextchat}
+              src={hirepulse}
               alt='e-commerce'
               className='ease-in-out duration-100 w-full hover:scale-105 object-contain object-center'
             />
           </div>
           <div className='flex flex-col ml-4 pt-2 items-start'>
             <div>
-              <h5 className='text-lg font-semibold text-start'>Next Chat</h5>
+              <h5 className='text-lg font-semibold text-start'>HirePulse</h5>
               <p className='flex items-center gap-1 text-blue-500 text-xs'>PERSONAL PROJECT</p>
             </div>
           </div>
           <div className='flex flex-col px-2 gap-3 h-auto'>
             <p className='text-sm text-start text-muted-foreground ml-1.5 mt-2'>
-              A web application to chat users and create group chats to share memories with another
-              users. Designed to give the users a great user experience with its light and dark
-              theme and modern looking feel.
+              A fullstack MERN application for managing and tracking job applications. Users can do
+              CRUD operations & admin can manage users job application. Responsive to all devices
+              with light/dark functionality.
             </p>
 
             {/* techstack icons */}
@@ -451,148 +450,14 @@ const Projects = () => {
                 </Tooltip>
               </TooltipProvider>
 
-              {/* NextJS Tooltip */}
+              {/* React Tooltip */}
               <TooltipProvider delayDuration={90} skipDelayDuration={60}>
                 <Tooltip>
                   <TooltipTrigger>
-                    <img src={NextJS} alt='nextjs-icon' className='w-[23.5px] mt-[3px]' />
+                    <img src={React} alt='react-leaflet-icon' className='w-[22px] mt-[3px]' />
                   </TooltipTrigger>
                   <TooltipContent>
-                    <p>NextJS</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-
-              {/* Convex Tooltip */}
-              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <img src={Convex} alt='convex-icon' className='w-[24px] mt-[3px]' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Convex</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-
-              {/* Clerk Tooltip */}
-              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <img src={Clerk} alt='nextjs-icon' className='w-[19px] mt-[3px]' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Clerk</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </div>
-
-            {/* source code & live preview */}
-            <div className='flex flex-col mb-1'>
-              {/* github source code button*/}
-              <a href='https://github.com/arvinbuid/next-chat' className='text-xs w-full'>
-                <button className='text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 mb-2 flex gap-1 items-center justify-center w-full'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='24'
-                    height='24'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    className='h-10 w-10 p-2 text-blue-600'
-                  >
-                    <path d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4'></path>
-                    <path d='M9 18c-4.51 2-5-2-7-2'></path>
-                  </svg>
-                  Source Code
-                </button>
-              </a>
-
-              {/* live preview */}
-              <a href='https://next-chat-teal-nine.vercel.app' className='text-xs w-full'>
-                <button className='text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 mb-2 flex gap-1 items-center justify-center w-full'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='24'
-                    height='24'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    className='h-10 w-10 p-2 text-blue-600'
-                  >
-                    <circle cx='12' cy='12' r='3'></circle>
-                    <path d='M21 12c0-3.9-4.5-9-9-9s-9 5.1-9 9 4.5 9 9 9 9-5.1 9-9z'></path>
-                  </svg>
-                  Live Preview
-                </button>
-              </a>
-            </div>
-          </div>
-        </aside>
-
-        {/* natours */}
-        <aside className='flex flex-col border rounded overflow-hidden h-auto w-full shadow m-auto max-w-[300px]'>
-          <div className='relative h-[180px] overflow-hidden flex justify-center'>
-            <img
-              src={natours}
-              alt='e-commerce'
-              className='ease-in-out duration-100 w-full hover:scale-105 object-contain object-center'
-            />
-          </div>
-          <div className='flex flex-col ml-4 pt-2 items-start'>
-            <div>
-              <h5 className='text-lg font-semibold text-start'>Natours</h5>
-              <p className='flex items-center gap-1 text-blue-500 text-xs'>PERSONAL PROJECT</p>
-            </div>
-          </div>
-          <div className='flex flex-col px-2 gap-3 h-auto'>
-            <p className='text-sm text-start text-muted-foreground ml-1.5 mt-2'>
-              A web application of the fictional company Natours to purchase and book a tour online.
-              The tours offer a different variety of tours, from hiking, camping, stargazing, and
-              many more.
-            </p>
-
-            {/* techstack icons */}
-            <div className='flex flex-wrap gap-1.5 col-span-3 my-2 ml-2 '>
-              {/* HTML Tooltip */}
-              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <img src={HTML} alt='html5-icon' className='w-[20px]' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>HTML5</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-
-              {/* CSS3 Tooltip */}
-              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <img src={CSS3} alt='css3-icon' className='w-[20px]' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>CSS3</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-
-              {/* Javascript */}
-              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <img src={JS} alt='javascript-icon' className='w-[22px] rounded-sm mt-[3px]' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Javascript</p>
+                    <p>React</p>
                   </TooltipContent>
                 </Tooltip>
               </TooltipProvider>
@@ -617,18 +482,6 @@ const Projects = () => {
                   </TooltipTrigger>
                   <TooltipContent>
                     <p>Express</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-
-              {/* Pug Tooltip */}
-              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <img src={Pug} alt='react-leaflet-icon' className='w-[22px] mt-[3px]' />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>Pug</p>
                   </TooltipContent>
                 </Tooltip>
               </TooltipProvider>
@@ -821,6 +674,156 @@ const Projects = () => {
                   className='text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 mb-2 flex gap-1 items-center justify-center w-full'
                   disabled={true}
                 >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='h-10 w-10 p-2 text-blue-600'
+                  >
+                    <circle cx='12' cy='12' r='3'></circle>
+                    <path d='M21 12c0-3.9-4.5-9-9-9s-9 5.1-9 9 4.5 9 9 9 9-5.1 9-9z'></path>
+                  </svg>
+                  Live Preview
+                </button>
+              </a>
+            </div>
+          </div>
+        </aside>
+
+        {/* next-chat */}
+        <aside className='flex flex-col border rounded overflow-hidden h-auto w-full shadow m-auto max-w-[300px]'>
+          <div className='relative h-[180px] overflow-hidden flex justify-center'>
+            <img
+              src={nextchat}
+              alt='e-commerce'
+              className='ease-in-out duration-100 w-full hover:scale-105 object-contain object-center'
+            />
+          </div>
+          <div className='flex flex-col ml-4 pt-2 items-start'>
+            <div>
+              <h5 className='text-lg font-semibold text-start'>Next Chat</h5>
+              <p className='flex items-center gap-1 text-blue-500 text-xs'>PERSONAL PROJECT</p>
+            </div>
+          </div>
+          <div className='flex flex-col px-2 gap-3 h-auto'>
+            <p className='text-sm text-start text-muted-foreground ml-1.5 mt-2'>
+              A web application to chat users and create group chats to share memories with another
+              users. Designed to give the users a great user experience with its light and dark
+              theme and modern looking feel.
+            </p>
+
+            {/* techstack icons */}
+            <div className='flex flex-wrap gap-1.5 col-span-3 my-2 ml-2 '>
+              {/* HTML Tooltip */}
+              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <img src={HTML} alt='html5-icon' className='w-[20px]' />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>HTML5</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              {/* CSS3 Tooltip */}
+              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <img src={CSS3} alt='css3-icon' className='w-[20px]' />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>CSS3</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              {/* Typescript */}
+              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <img
+                      src={Typescript}
+                      alt='javascript-icon'
+                      className='w-[22px] rounded-sm mt-[3px]'
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Typescript</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              {/* NextJS Tooltip */}
+              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <img src={NextJS} alt='nextjs-icon' className='w-[23.5px] mt-[3px]' />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>NextJS</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              {/* Convex Tooltip */}
+              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <img src={Convex} alt='convex-icon' className='w-[24px] mt-[3px]' />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Convex</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              {/* Clerk Tooltip */}
+              <TooltipProvider delayDuration={90} skipDelayDuration={60}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <img src={Clerk} alt='nextjs-icon' className='w-[19px] mt-[3px]' />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Clerk</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+
+            {/* source code & live preview */}
+            <div className='flex flex-col mb-1'>
+              {/* github source code button*/}
+              <a href='https://github.com/arvinbuid/next-chat' className='text-xs w-full'>
+                <button className='text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 mb-2 flex gap-1 items-center justify-center w-full'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='h-10 w-10 p-2 text-blue-600'
+                  >
+                    <path d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4'></path>
+                    <path d='M9 18c-4.51 2-5-2-7-2'></path>
+                  </svg>
+                  Source Code
+                </button>
+              </a>
+
+              {/* live preview */}
+              <a href='https://next-chat-teal-nine.vercel.app' className='text-xs w-full'>
+                <button className='text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 mb-2 flex gap-1 items-center justify-center w-full'>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     width='24'
