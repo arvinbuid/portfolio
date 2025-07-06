92910cef6aca26c20e2448d47409f01c7a0a1bac
Arvin Buid <arvinbuid01@gmail.com>
2025-07-06 15:54:24 +0800

    Implement curve svg animation to Preloader component.

--- components/preloader/Preloader.tsx
+++ components/preloader/Preloader.tsx
@@ -11,7 +11,14 @@ const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallå", "Guten ta
 
 const Preloader = () => {
     const [index, setIndex] = useState(0);
+    const [dimension, setDimension] = useState({ width: 0, height: 0 });
 
+    // Effect to get the width and height of the window
+    useEffect(() => {
+        setDimension({ width: window.innerWidth, height: window.innerHeight })
+    }, [])
+
+    // Effect to change the words every 150 milliseconds
     useEffect(() => {
         if (index == words.length - 1) return;
 
@@ -21,9 +28,43 @@ const Preloader = () => {
         return () => clearTimeout(timer);
     }, [index])
 
+    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width / 2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
+    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height}  L0 0`
+
+    type Curve = {
+        initial: {
+            d: string
+        },
+        exit: {
+            d: string;
+            transition: {
+                duration: number;
+                ease: [number, number, number, number];
+                delay: number;
+            }
+        }
+    }
+
+    const curve: Curve = {
+        initial: {
+            d: initialPath
+        },
+        exit: {
+            d: targetPath,
+            transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 }
+        }
+    }
+
     return (
         <motion.div variants={slideUp} initial="initial" exit="exit" className={styles.introduction}>
             <motion.p variants={opacity} initial="initial" animate="enter">{words[index]}</motion.p>
+            {
+                dimension.height > 0 && (
+                    <svg>
+                        <motion.path variants={curve} initial="initial" exit="exit" d={initialPath}></motion.path>
+                    </svg>
+                )
+            }
         </motion.div>
     );
 }
